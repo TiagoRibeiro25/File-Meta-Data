3806e44042dd8884ba4700b7d2e9fd65118a62fe
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-02-08 00:38:07 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", (req, res) => res.sendFile(process.cwd() + "/views/index.html"));
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+	res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size });
+});
+
+app.listen(port, () => console.log("Your app is listening on port " + port));
